ec38d91dd5ce3f8b20aba5f019a8ad3c9ee65ece
Alexander van Trijffel <alexander@structura.ws>
2021-06-12 08:35:19 +0200

    feat: project structure

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import log from './log'
+log.info('We are up and running 🚀')
---
+++ src/log.ts
@@ -0,0 +1,25 @@
+import winston, { format, transports } from 'winston'
+import path from 'path'
+import { envString } from './env'
+
+const logFormat = format.printf((info) => {
+  return `${info.timestamp} ${info.level}: ${info.message} ${
+    info.metadata && Object.keys(info.metadata).length > 0 ? JSON.stringify(info.metadata) : ''
+  } `
+})
+
+const logger = winston.createLogger({
+  level: envString('NODE_ENV') === 'production' ? 'info' : 'debug',
+  format: format.combine(
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.metadata({ fillExcept: ['message', 'level', 'timestamp', 'label'] })
+  ),
+  transports: [
+    new transports.Console({
+      format: format.combine(format.colorize(), logFormat)
+    })
+  ],
+  exitOnError: false
+})
+
+export default logger
